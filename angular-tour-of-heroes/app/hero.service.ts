eb4cfb52045e8ec854b5e5452065469d678b717b
Sherman Chen <wchen@progress.com>
2016-12-14 16:53:20 +0800

    // angular-tour-of-heroes
    Added the angular tour of heroes sample app.

---
+++ angular-tour-of-heroes/app/hero.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import {Hero} from './hero';
+
+@Injectable()
+export class HeroService {
+    private heroesUrl = 'api/heroes';
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private http: Http) { }
+
+    getHero(id: Number): Promise<Hero> {
+        // return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+        const url = `${this.heroesUrl}/${id}`;
+
+        return this.http.get(url).toPromise().then(response => response.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl).toPromise().then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+
+        return this.http.put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise().then(() => hero).catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occured', error);
+        return Promise.reject(error.message || error);
+    }
+}
---
+++ angular-tour-of-heroes/app/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
+import { Hero } from './hero';
+import {HeroService} from './hero.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-heroes',
+    templateUrl: 'heroes.component.html',
+    styleUrls: ['heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit { 
+    selectedHero: Hero;
+    heroes: Hero[];
+
+    constructor(private router: Router, private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    gotoDetail() : void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+}
